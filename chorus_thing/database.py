6c7261f9975ca0662f21cb49a8f3222885352f0e
Matthew Michal <matthew.michal11@gmail.com>
2024-08-10 19:21:24 +0000

    added event table

--- chorus_thing/database.py
+++ chorus_thing/database.py
@@ -19,3 +19,20 @@ class BaseTable(AbstractConcreteBase, ChorusThing):
     id: Mapped[int] = mapped_column(primary_key=True)
     created: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC))
     modified: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC), onupdate=datetime.now(tz=UTC))
+
+
+class Event(BaseTable):
+    """Table of Choral Events."""
+
+    __tablename__ = "event"
+
+    paid_event: Mapped[bool] = mapped_column(default=False)
+    location: Mapped[str]
+    event_time: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC), onupdate=datetime.now(tz=UTC))
+    necessary_size: Mapped[int]
+    choral_arrangement: Mapped[str] = mapped_column(default="SATB")
+
+    def __repr__(self) -> str:
+        """String representation of table."""
+        return f"""Event(event_time={self.event_time}, location={self.location}, paid_event={self.paid_event},
+        choral_arrangement={self.choral_arrangement}, necessary_size={self.necessary_size})"""
