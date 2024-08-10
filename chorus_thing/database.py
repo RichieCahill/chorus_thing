2421aecb20f8d0fe824c81821235770f3aae558a
Matthew Michal <matthew.michal11@gmail.com>
2024-08-10 18:47:59 +0000

    added event table

--- chorus_thing/database.py
+++ chorus_thing/database.py
@@ -5,6 +5,7 @@ from datetime import UTC, datetime
 from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import AbstractConcreteBase
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import ForeignKey
 
 
 class ChorusThing(DeclarativeBase):
@@ -19,3 +20,18 @@ class BaseTable(AbstractConcreteBase, ChorusThing):
     id: Mapped[int] = mapped_column(primary_key=True)
     created: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC))
     modified: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC), onupdate=datetime.now(tz=UTC))
+
+
+class Event(BaseTable):
+    """Table of Choral Events."""
+
+    __tablename__ = "event"
+
+    paid_event: Mapped[bool] = mapped_column(default=False)
+    location: Mapped[str]
+    event_time: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC), onupdate=datetime.now(tz=UTC))
+    necessary_size: Mapped[int]
+
+    def __repr__(self) -> str:
+        """String representation of table."""
+        return f"Event(event_time={self.event_time}, location={self.location}, paid_event={self.paid_event}, necessary_size={self.necessary_size})"
