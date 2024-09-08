865847ba7d1cf472cec21aa3f3361da08265e4ee
Matthew Michal <matthew.michal11@gmail.com>
2024-09-08 16:39:47 +0000

    Clean up of BaseColumns and main

--- chorus_thing/__main__.py
+++ chorus_thing/__main__.py
@@ -2,8 +2,14 @@
 
 from __future__ import annotations
 
+from sqlalchemy import create_engine
+
 from chorus_thing.app import create_app
+from chorus_thing.database import Chorusitem
 
 if __name__ == "__main__":
+    engine = create_engine("sqlite://", echo=True)
+    Chorusitem.metadata.create_all(engine)
+
     APP = create_app()
     APP.run(host="0.0.0.0", port=5000, debug=True)  # noqa: S104
